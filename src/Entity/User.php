bd7a16f6063ffc77a082ff84728cfafc5253ccb2
othmane <badrelqadmirio@gmail.com>
2026-01-02 20:34:17 +0100

    update auth

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,11 +7,13 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'users')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     // ----------------- Champs -----------------
@@ -59,6 +61,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->emprunts = new ArrayCollection();
         $this->empruntsValides = new ArrayCollection();
         $this->roles = ['ROLE_USER'];
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
     }
 
     // ----------------- Security -----------------
