e2221f5015653471d91dd43153edd4190e9daea1
ahmed ennouri <ahmedenouri7@gmail.com>
2026-01-10 10:51:23 +0100

    delete category and update livre repository

--- migrations/Version20260110094600.php
+++ migrations/Version20260110094600.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260105134014 extends AbstractMigration
+final class Version20260110094600 extends AbstractMigration
 {
     public function getDescription(): string
     {
